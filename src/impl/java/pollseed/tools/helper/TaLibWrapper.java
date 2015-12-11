52b53bd3560ee14459e5ce1da786a7bf3ae9722b
pollseed <cb.so.devope+0001@gmail.com>
2015-12-11 23:11:49 +0900

    add Wrapper

---
+++ src/impl/java/pollseed/tools/helper/TaLibWrapper.java
@@ -0,0 +1,71 @@
+package pollseed.tools.helper;
+
+import pollseed.tools.helper.interfaces.CommandPrinter;
+
+import com.tictactec.ta.lib.Core;
+import com.tictactec.ta.lib.MAType;
+import com.tictactec.ta.lib.MInteger;
+import com.tictactec.ta.lib.RetCode;
+
+public class TaLibWrapper implements CommandPrinter {
+
+    private static final Core C = new Core();
+    private static final CommandPrinter P = new CommandPrinter() {};
+
+    private static MInteger __outBegIdx = new MInteger();
+    private static MInteger __outNBElement = new MInteger();
+
+    private static class StandardVal {
+        private static final int START_IDX = 0;
+        private static final int END_IDX = 399;
+        private static final double[] IN_REAL = new double[400];
+        private static final int OPT_IN_TIME_PERIOD = 30;
+        private static final double[] OUT_REAL = new double[400];
+    }
+
+    private static class AdvancedVal {
+        private static final int OPT_IN_FAST_PERIOD = 100;
+        private static final int OPT_IN_SLOW_PERIOD = 100;
+        private static final int OPT_IN_SIGNAL_PERIOD = 100;
+        private static final int OPT_IN_TIME_PERIOD = 100;
+        private static final double[] OUT_MACD = new double[400];
+        private static final double[] OUT_MACD_SIGNAL = new double[400];
+        private static final double[] OUT_MACD_HIST = new double[400];
+    }
+
+    public static void main(String[] args) {
+        execute(() -> {
+            return C.sma(StandardVal.START_IDX, StandardVal.END_IDX, StandardVal.IN_REAL, StandardVal.OPT_IN_TIME_PERIOD, __outBegIdx,
+                    __outNBElement, StandardVal.OUT_REAL);
+        });
+        execute(() -> {
+            return C.rsi(StandardVal.START_IDX, StandardVal.END_IDX, StandardVal.IN_REAL, StandardVal.OPT_IN_TIME_PERIOD, __outBegIdx,
+                    __outNBElement, StandardVal.OUT_REAL);
+        });
+        execute(() -> {
+            return C.macd(StandardVal.START_IDX, StandardVal.END_IDX, StandardVal.IN_REAL, AdvancedVal.OPT_IN_FAST_PERIOD,
+                    AdvancedVal.OPT_IN_SLOW_PERIOD, AdvancedVal.OPT_IN_SIGNAL_PERIOD, __outBegIdx, __outNBElement, AdvancedVal.OUT_MACD,
+                    AdvancedVal.OUT_MACD_SIGNAL, AdvancedVal.OUT_MACD_HIST);
+        });
+        execute(() -> {
+            return C.movingAverage(StandardVal.START_IDX, StandardVal.END_IDX, StandardVal.IN_REAL, AdvancedVal.OPT_IN_TIME_PERIOD, MAType.Kama,
+                    __outBegIdx, __outNBElement, StandardVal.OUT_REAL);
+        });
+    }
+
+    private static void execute(TaLib t) {
+        if (RetCode.Success == t.run()) {
+            P.ln(__outBegIdx.value);
+            P.ln(__outNBElement.value);
+        }
+    }
+
+    @FunctionalInterface
+    private interface TaLib {
+        RetCode run();
+    }
+
+    private TaLibWrapper() throws AssertionError {
+        new AssertionError("cannot create instance.");
+    }
+}
---
+++ src/test/java/pollseed/tools/helper/KuromojiWrapperTest.java
@@ -0,0 +1,35 @@
+package pollseed.tools.helper;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.atilika.kuromoji.jumandic.Token;
+
+public class KuromojiWrapperTest {
+    static KuromojiWrapper KW = new KuromojiWrapper();
+
+    @Test
+    public void test_surfaces() {
+        final Queue<String> surfacesTrue = new LinkedList<String>() {
+            private static final long serialVersionUID = 1L;
+            {
+                add("ほ");
+                add("げ");
+                add("ほ");
+                add("げ");
+            }
+        };
+        KW.surfaces("ほげほげ").forEach(v -> Assert.assertEquals(v, surfacesTrue.poll()));
+    }
+
+    @Test
+    public void test_features() {
+        final Queue<Token> tokens = new LinkedList<Token>();
+        KW.token("ほげほげ").forEach(v -> tokens.add(v));
+        KW.allFeatures("ほげほげ").forEach(v -> Assert.assertEquals(v, tokens.poll().getAllFeatures()));
+    }
+}
